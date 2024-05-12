940d5d2920584e172432d91acabc96eb6240a584
Yukimi Otagiri <Yukimi.otagiri@gmail.com>
2024-05-12 22:14:47 +0900

    Added error

--- forms/login/LoginForm.js
+++ forms/login/LoginForm.js
@@ -20,8 +20,34 @@ import userStore from "../../stores/userStore";
 const LoginForm = observer(() => {
     const router = useRouter();
     const onSuccess = async (form) => {
+      
 
         const values = form.values();
+
+        // try {
+        //     const response = await Login(email, password);
+        //     localStorage.setItem('accessToken', response.data.key);
+        //     const userData = await GetUser(response.data.key);
+        //     UserStore.authenticate(userData); // save the user details in localStorage
+        //     form.clear(); // reset the form
+        //     setLoading(false);
+        //     navigate('/');
+        // } catch (error) {
+        //     setLoading(false);
+
+        //     if (error.response.data['email']) {
+        //         form.select('email').invalidate(error.response.data['email'][0]);
+        //     } else if (error.response.data['password']) {
+        //         form.select('password').invalidate(error.response.data['password'][0]);
+        //     } else if (error.response.data['new_password2']) {
+        //         form.select('new_password2').invalidate(error.response.data['new_password2'][0]);
+        //     } else if (error.response.data['non_field_errors']) {
+        //         form.invalidate(error.response.data['non_field_errors'][0]);/////Check it out
+        //         ToastError(error.response.data['non_field_errors'][0]) // toast the user
+        //     } else {
+        //         ToastError('Unknown Error') // toast the user
+        //     }
+        // }
         try {
             const response = await Login(values.username, values.email, values.password);
             console.log(response)
@@ -32,6 +58,10 @@ const LoginForm = observer(() => {
             router.push('/'); 
     
         } catch (error) {
+            console.log("error.response.data is ", error.response.data.non_field_errors[0])
+            if (error.response.data.non_field_errors){
+                form.invalidate(error.response.data.non_field_errors[0]) 
+            }
             console.log(error)
         }
             //Login(values.username, values.email, values.password).then(response => {
@@ -95,6 +125,7 @@ const LoginForm = observer(() => {
                             </Form.Group>
 
                             <Button type="submit" onClick={handleSubmit}>Submit</Button>
+                            <p>{form.error}</p>
                             {/* <Button type="button" onClick={form.onClear}>Clear</Button>
                             <Button type="button" onClick={form.onReset}>Reset</Button> */}
                             
